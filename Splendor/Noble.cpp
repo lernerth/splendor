65abf28fd899f7b5608f96bd019723e57e9a7a9d
Solene Desvaux De Marigny <solene.desvaux-de-marigny@etu.utc.fr>
2021-12-16 09:02:15 +0000

    Upload New File

---
+++ Splendor/Noble.cpp
@@ -0,0 +1,51 @@
+#include "Noble.h"
+
+//constructeur
+Noble::Noble() {
+    id_noble = 0;
+    for(int i = 0; i < 5; ++i){
+        cout[i] = 0;
+    }
+    point_prestige = 0;
+}
+
+
+//récupérateur
+int Noble::getIdNoble(){
+    return this->id_noble;
+}
+
+int Noble::getCout(int index){
+    switch(index){
+        case '0':
+            return cout[0];
+        case '1':
+            return cout[1];
+        case '2':
+            return cout[2];
+        case '3':
+            return cout[3];
+        case '4':
+            return cout[4];
+    }
+}
+
+int Noble::getPtPrestige(){
+    return this->point_prestige;
+}
+
+
+//setters
+void Noble::setIdNoble(int id_Noble){
+    this->id_noble = id_noble;
+}
+
+void Noble::setCout(int *tab_cout){
+    for(int i = 0; i < 5; i++){
+        cout[i] = *tab_cout[i];
+    }
+}
+
+void Noble::setPtPrestige(int point_prestige){
+    this->point_prestige = point_prestige;
+}
