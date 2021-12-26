68a8c1b78c44342916556a173b2aa5eff09eda2c
Gabrielle Van De Vijver <gabrielle.van-de-vijver@etu.utc.fr>
2021-12-26 10:44:24 +0000

    Update Pioche_carte.h

--- Splendor/Pioche_carte.h
+++ Splendor/Pioche_carte.h
@@ -9,20 +9,27 @@
 
 class Pioche_carte {
 public:
-    //constructeur
+    //constructeur-destructeur
     Pioche_carte();
-
-    //récupérateur
-    int getNbCarte();
-    int getNiveau();
+    ~Pioche_carte()	{delete[] cartes;}//edit
 
     //Setters
-    void setNbCarte(int nbcarte);
     void setNiveau(int niv);
+    void setNbCarte();
+
+    //récupérateurs
+    int getNbCarte()const {return nb;}//edit
+    int getNiveau() const {return niveau;}//edit
+
+    //méthodes
+    size_t getNbCartes() {return nb_cartes;}//edit
+    bool estVide() const {return nb_cartes == 0;}//edit
+    const Carte& piocher();//edit
 
 private:
-    int nb_carte;
+    int nb_cartes;
     int niveau;
+    const Carte** cartes = nullptr;//edit
 };
 
 #endif
