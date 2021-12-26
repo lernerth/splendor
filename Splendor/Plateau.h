c45f2ebb1c2d41a2f0947d1ca41790e99e5a4357
Gabrielle Van De Vijver <gabrielle.van-de-vijver@etu.utc.fr>
2021-12-26 14:36:43 +0000

    Update Plateau.h

--- Splendor/Plateau.h
+++ Splendor/Plateau.h
@@ -14,21 +14,26 @@
 
 using namespace std;
 class Plateau {
+//Suggestion : plutôt que des id entiers, fonctionner avec des pointeurs (à initialiser dans le cpp avec nullptr)
 public:
     //Constructeur
     Plateau();
 
     int getNoble(int index_noble);
+    //Noble* getNoble(int indice_noble);
     int getIdCarteGrille(int niveau, int indice);
+    //Carte* getCarteGrille(int niveau, int indice);
 
     void setNoble(int tab_noble[5]);
+    //void setNoble (Noble* tab_noble[5]):
     void setGrille(int tab_grille[3][4]);
-    void remplacerCarte(int niveau, int indice);
+    //void setGrille(Carte* tab_grille[3][4])
+    //void remplacerCarte(int niveau, int indice);->Controleur
 
 private:
     int grille[3][4];
     int noble[5];
-
+    
 };
 
 #endif // PLATEAU_H
