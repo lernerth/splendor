6d4c5fb3276c7d58f917808767e42875b0531838
Pierre Fagot <pierrefagot2000@gmail.com>
2021-12-29 15:16:22 +0100

    update Plateau, passage en ptr

--- Splendor/Plateau.h
+++ Splendor/Plateau.h
@@ -4,37 +4,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <Carte.h>
-#include <Noble.h>
-#include <Pioche_carte.h>
+#include "Carte.h"
+#include "Noble.h"
+#include "Pioche_carte.h"
+#include "pioche_noble.h"
 #include <time.h>
 #include <stdlib.h>
-#include <Partie.h>
+#include "Partie.h"
 #include "Exception.h"
 
 
 using namespace std;
 class Plateau {
-//Suggestion : plutôt que des id entiers, fonctionner avec des pointeurs (à initialiser dans le cpp avec nullptr)
 public:
     //Constructeur
     Plateau();
 
-    int getNoble(int index_noble);
-    //Noble* getNoble(int indice_noble);
-    int getIdCarteGrille(int niveau, int indice);
-    //Carte* getCarteGrille(int niveau, int indice);
+    Noble* getNoble(int indice_noble) {return noble[indice_noble];};
+    Carte* getCarteGrille(int niveau, int indice) {return grille[niveau][indice];};
 
-    void setNoble(int tab_noble[5]);
-    //void setNoble (Noble* tab_noble[5]):
-    void setGrille(int tab_grille[3][4]);
-    //void setGrille(Carte* tab_grille[3][4])
-    //void remplacerCarte(int niveau, int indice);->Controleur
 
 private:
-    int grille[3][4];
-    //Carte* grille[3][4];
-    int noble[5];
-    //Noble* grille[5] 
+
+    Carte* grille[3][4];
+    Noble* noble[5];
 };
 #endif // PLATEAU_H
