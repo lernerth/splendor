ba4ab196605b1ba2ed14d69cd4d7d65831a11dea
Solene Desvaux De Marigny <solene.desvaux-de-marigny@etu.utc.fr>
2021-12-30 17:20:52 +0000

    Replace joueur.cpp

--- Splendor/joueur.cpp
+++ Splendor/joueur.cpp
@@ -1,5 +1,63 @@
 #include "joueur.h"
 
+int Joueur::getNbJetonsTot(){
+    int t = jetons.value("blanc", 0)+jetons.value("bleu", 0)+jetons["noir"]+jetons["rouge"]+jetons["vert"]+jetons["jaune"];
+    return t;
+}
+
+//Ajoute la carte entrée dans la réserve s'il reste de la place.
+void Joueur::ajouter_carte_reserve(Carte* carte, Carte** cartes_reservees) {
+    for(int i = 0; i <3 ; ++i) {
+        if(cartes_reservees[i] == 0){
+            cartes_reservees[i] = carte;
+            break;
+        }
+        else if(i==2){
+            std::cout <<"Impossible réserve déjà pleine";
+            break;
+        }
+    }
+}
+
+
+//Retire une carte de la reserve. ATTENTION : il faut l'acheter après
+void Joueur::retirer_carte_reserve(Carte* carte, Carte** cartes_reservees){
+    //a faire
+}
+
+
+//Achète une carte
+void Joueur::acheter_carte(Carte* carte, Carte** cartes_achetees) {
+    int i = 0;
+    while(cartes_achetees[i] != 0) {
+        i++;
+        }
+    cartes_achetees[i] = carte;
+    //ajouter les prestige
+    //ajouter les bonus
+}
+
+
+
+//Donne un noble au joueur
+void Joueur::recupérer_noble(Carte* carte, Noble** noble) {
+    int i = 0;
+    while(noble[i] != 0) {
+        i++;
+        }
+    cartes_achetees[i] = carte;
+    //ajouter les prestige
+    //ajouter les bonus
+}
+
+/*
+void Joueur::ajouter_jeton(Couleur c){
+    jetons[c]++;
+}
+*/
+
+
+
 //CONSTRUCTEUR
 //Joueur():initialise les attributs de la classe à 0/nullptr
 
@@ -63,32 +121,12 @@ void Joueur::setBonus(int indice){
     bonus[indice]++;
 }
 
-//Set_ReserveCartes
-//Ajoute la carte entrée dans la réserve s'il reste de la place.
-void Joueur::setReserveCartes(int reserve_carte) {
-    for (int i = 0; i <3 ; ++i) {
-        if(cartes_reservees[i] == 0){
-            cartes_reservees[i] = reserve_carte;
-            break;
-        }
-        else if(i==2){
-            std::cout <<"Impossible réserve déjà pleine";
-            break;
-        }
-    }
-}
 
 //Set_prestige(int points_supp): ajoute un nombre de donné au prestige du joueur.
 void Joueur::setPrestige(int nb_points){
     prestige+= nb_points;
 }
 
-
-//remove_Reserve
-//acheter une carte de la réserve
-void Joueur::removeReserve(int indice){
-    cartes_reservees[indice] = 0;
-}
 
 //set_Id (string id)
 //définit l'identifiant du joueur.
