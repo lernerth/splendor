8a07c28db541b6a873a0a3630c2591cf31395bf3
Solene Desvaux De Marigny <solene.desvaux-de-marigny@etu.utc.fr>
2022-01-02 16:51:23 +0000

    Replace Partie.h

--- Splendor/Partie.h
+++ Splendor/Partie.h
@@ -21,19 +21,16 @@ class Partie {
 public:
     Partie(size_t n, QString j[3]);
     size_t getNbJoueurs() {return Nbjoueur;};
-    const Joueur& getJoueur(size_t numJ);
-    void PiocherJetons(Couleur c1, Couleur c2, Couleur c3, Joueur* joueur, Controleur* controleur);
-    Carte& AcheterCarte(Carte* carte, Joueur* joueur, Controleur* controleur);
-    Carte& ReserverCarte(Carte* carte, Joueur* joueur, Controleur* controleur);
-    void FinDePartie(); //argument à def
-    /*TO DO
-     * vérifie si un joueur est éligible à la visite d'un noble
-     * puis si un joueur à += de 15 de prestige
-     *  -> si oui dernier tour de jeu
-     *  -> si non la partie continue
-    */
+    Joueur& getJoueur(size_t numJ);
+    Controleur* getControleur() {return cont;}
+    void PiocherJetons(Couleur c1, Couleur c2, Couleur c3, Joueur& joueur, Controleur* controleur);
+    Carte& AcheterCarte(Carte* carte, Joueur& joueur, Controleur* controleur);
+    Carte& ReserverCarte(Carte* carte, Joueur& joueur, Controleur* controleur);
+    void FinDePartie();
 
 private:
+    friend class Controleur;
+    Controleur* cont;
     size_t Nbjoueur;
     Joueur* ListeJoueur[3];
 
