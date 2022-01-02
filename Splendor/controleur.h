0bc17f9c407722accb66ffb1d1332aec57847e45
Solene Desvaux De Marigny <solene.desvaux-de-marigny@etu.utc.fr>
2022-01-02 16:53:24 +0000

    Replace controleur.h

--- Splendor/controleur.h
+++ Splendor/controleur.h
@@ -4,47 +4,38 @@
 #include "Carte.h"
 #include "Noble.h"
 #include "Plateau.h"
+#include "Carte.h"
+#include "Noble.h"
 
- class Controleur {
+ class Controleur{
 	public:
-    //controleur-destructeur
-        Controleur(Carte** c1, Carte** c2, Carte** c3, Noble* n[10]): p1(c1),p2(c2),p3(c3), plateau(new Plateau) {
-        //initialisation de la pile de jetons
-            int jetons[6];
-            for (int i = 0; i <5; ++i) {
-                jetons[i]= 7;
-            }
-            jetons[5]=5;
-		}
+        //constructeur-destructeur
+        Controleur();
+        //~Controleur() { delete pn; } //Pourquoi delete que la pioche noble ??
 
-        // ~Controleur() { delete pn; } Pourquoi delete que la pioche noble ??
-    //getters
-		const Plateau& getPlateau() const { return plateau; }
-       // const Pioche_jetons& getJetons() const { return pj; } //Pioche de jetons n'existe pas !
+        //getters
+        const Plateau& getPlateau() const { return plateau; }
         const Pioche_carte& getPioche(int niveau) const;
         const Noble* getNobles(int i) const {return pn[i];}
-    //Singleton
-		Controleur(const Controleur& c) = delete;
-		Controleur& operator=(const Controleur& c) = delete;
 
-    //Methodes
-    void initialiser_Plateau(int nb_joueurs);//initialiser la grille et la rangée
-    //remplacer une carte développement du tableau
-    Carte& retirer_carte(Carte* carte);
-    //retirer une carte noble
-    Noble& retirer_Noble(Noble* noble);
-    //retirer des jetons de la pioche centrale
-    void retirer_Jetons(Couleur c);
-    //rendre des jetons à la pioche centrale
-    void rendre_Jetons(Couleur c);
+        //Singleton
+        Controleur(const Controleur& c) = delete;
+        Controleur& operator=(const Controleur& c) = delete;
+
+        //Methodes
+        void initialiser_Plateau(int nb_joueurs);//initialiser la grille et la rangée
+        void retirer_carte(Carte* carte);     //remplacer une carte développement du tableau
+        void retirer_Noble(Noble* noble);     //retirer une carte noble
+        void retirer_Jetons(Couleur c);         //retirer des jetons de la pioche centrale
+        void rendre_Jetons(Couleur c);          //rendre des jetons à la pioche centrale
 
 	private:
         friend class Partie;
-        Carte** p1;
-        Carte** p2;
-        Carte** p3;
+        Pioche_carte& p1;
+        Pioche_carte& p2;
+        Pioche_carte& p3;
         QMap<Couleur, int> pj;
-		Plateau plateau;
+        Plateau& plateau;
         const Noble* pn[10];
 	};
 
